226d47c314158527e469dfc1742c896a2d1c82f4
Ali MoshkinFaam <ali.fam0079@gmail.com>
2022-09-08 22:45:18 +0430

    Links

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,22 @@
+import Link from 'next/link'
+import {Fragment} from "react";
+
+
 // our-domain/news
 function NewsPage(){
 
-    return(<h1>News Page</h1>)
+    return(
+        <Fragment>
+            <ul>
+                <li>
+                    <Link href="/news/nextjs-is-a-great-framework">NextJS is a great framework</Link>
+                </li>
+            </ul>
+            <h1>News Page</h1>
+        </Fragment>
+
+
+    )
 }
 
 export default NewsPage;
