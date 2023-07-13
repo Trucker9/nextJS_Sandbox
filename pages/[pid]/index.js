96bef3bf94bc25d4f10fc70a83ef123c5f656b54
unholy <>
2023-07-13 14:34:30 +0330

    404 and fallback true

--- pages/[pid]/index.js
+++ pages/[pid]/index.js
@@ -5,6 +5,11 @@ import fs from "fs/promises";
 function ProductDetailPage(props) {
   const { fetchedProduct } = props;
 
+    // Because fallback is true, we add this loading page for the time that
+    // the actual page is being rendered. after the render finishes, the actual
+    // page will be loaded for the user.
+    if (!fetchedProduct) return  <p>Loading</p>
+
   return (
     <>
       <h1>{fetchedProduct.title}</h1>
@@ -24,6 +29,11 @@ export async function getStaticProps(context) {
   const productId = params.pid;
 
   const product = data.products.find((pr) => productId === pr.id);
+  // If failed to find a product for an id do not return the page with missing
+  // data. instead return 404
+  if (!product){
+    return {notFound: true}
+  }
 
   return {
     props: {
@@ -37,13 +47,13 @@ export async function getStaticPaths() {
   const ids = data.products.map((el) => {
     el.id;
   });
-  
   const extractedPaths = ids.map((el) => ({ params: { pid: el } }));
-
   return {
     paths: [extractedPaths],
 
-    fallback: "blocking",
+    // By setting this to true, the page will be loaded for any entered "pid"
+    // which may not be in the data we fetch and causes error.
+    fallback: true,
   };
 }
 
